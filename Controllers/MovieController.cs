74bf94b1c2263363bca16a181fc5c3e290b02dad
pogi <harlybusinessmind@gmail.com>
2024-09-25 23:22:11 +0800

    http put

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -81,31 +81,115 @@ namespace MovieAPIDemo.Controllers
 
             try
             {
-                var actors = _context.Persons.Where(x => model.Persons.Contains(x.Id)).ToList();
-                if (actors.Count != model.Persons.Count)
+                if (ModelState.IsValid)
+                {
+                    var actors = _context.Persons.Where(x => model.Persons.Contains(x.Id)).ToList();
+                    if (actors.Count != model.Persons.Count)
+                    {
+                        response.Status = false;
+                        response.Message = "Invalid Actor assigned";
+                        return BadRequest(response);
+                    }
+
+                    var postedModel = new Movie()
+                    {
+                        Title = model.Title,
+                        ReleaseDate = model.ReleaseDate,
+                        Language = model.Language,
+                        CoverImage = model.CoverImage,
+                        Description = model.Description,
+                        Persons = actors
+                    };
+
+                    _context.Movies.Add(postedModel);
+                    _context.SaveChanges();
+
+                    response.Status = true;
+                    response.Message = "Success";
+                    response.Data = postedModel;
+                    return Ok(response);
+                }
+                else
                 {
                     response.Status = false;
-                    response.Message = "Invalid Actor assigned";
+                    response.Message = "Validation Failed";
+
                     return BadRequest(response);
                 }
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = "Something went wrong!";
 
-                var postedModel = new Movie()
+                return BadRequest(response);
+            }
+        }
+
+
+        [HttpPut]
+        public IActionResult Put(CreateMovieViewModel model)
+        {
+            BaseResponseEntity response = new BaseResponseEntity();
+
+            try
+            {
+                if (ModelState.IsValid)
                 {
-                    Title = model.Title,
-                    ReleaseDate = model.ReleaseDate,
-                    Language = model.Language,
-                    CoverImage = model.CoverImage,
-                    Description = model.Description,
-                    Persons = actors
-                };
+                    if (model.Id <= 0)
+                    {
+                        response.Status = false;
+                        response.Message = "Invalid Actor assigned";
+                        return BadRequest(response);
+                    }
+                    var actors = _context.Persons.Where(x => model.Persons.Contains(x.Id)).ToList();
+                    if (actors.Count != model.Persons.Count)
+                    {
+                        response.Status = false;
+                        response.Message = "Invalid Actor assigned";
+                        return BadRequest(response);
+                    }
+                    var movieDetails = _context.Movies.Include(x => x.Persons).Where(x => x.Id == model.Id).FirstOrDefault();
 
-                _context.Movies.Add(postedModel);
-                _context.SaveChanges();
+                    if (movieDetails == null)
+                    {
+                        response.Status = false;
+                        response.Message = "Invalid Movie Record";
+                        return BadRequest(response);
+                    }
 
-                response.Status = true;
-                response.Message = "Success";
-                response.Data = postedModel;
+                    movieDetails.Title = model.Title;
+                    movieDetails.ReleaseDate = model.ReleaseDate;
+                    movieDetails.Language = model.Language;
+                    movieDetails.CoverImage = model.CoverImage;
+                    movieDetails.Description = model.Description;
 
+                    //find removed actors persons
+                    var removedPersons = movieDetails.Persons.Where(x => !model.Persons.Contains(x.Id)).ToList();
+                    foreach (var person in removedPersons)
+                    {
+                        movieDetails.Persons.Remove(person);
+                    }
+                    //find added actors person
+                    var addedPersons = actors.Except(movieDetails.Persons).ToList();
+                    foreach (var person in addedPersons)
+                    {
+                        movieDetails.Persons.Add(person);
+                    }
+                    _context.SaveChanges();
+
+                    response.Status = true;
+                    response.Message = "Updated Successfuly";
+                    response.Data = movieDetails;
+                    return Ok(response);
+                }
+                else
+                {
+                    response.Status = false;
+                    response.Message = "Validation Failed";
+
+                    return BadRequest(response);
+                }
             }
             catch (Exception ex)
             {
